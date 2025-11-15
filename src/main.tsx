b698da8ec92b5ecb3f02bc980062d7ec9a75ae39
nexur <nexur@gmail.com>
2025-11-15 17:16:00 +0800

    repair

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./app/App";
 import "./styles/tailwind.css";
 
+if (typeof window !== "undefined" && window.location.pathname.endsWith("index.html")) {
+  const { pathname, search, hash } = window.location;
+  const nextPathname = pathname.replace(/index\.html$/, "") || "/";
+  const nextUrl = `${nextPathname}${search}${hash}`;
+
+  window.history.replaceState(null, "", nextUrl);
+}
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
